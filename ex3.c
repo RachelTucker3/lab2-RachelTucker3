4cb2b8f055133c6167f5e2b535311a603a0665f5
Rachel Tucker <rach1234@live.unc.edu>
2019-02-04 16:46:13 +0000

    replace this with a useful message

--- ex3.c
+++ ex3.c
@@ -3,9 +3,30 @@
 	int main()
 	{
 
-	int a, b, c, d, e, f;
+		int a, b, c, d, e, f;
 
-	printf("Enter six integers: \n");
-	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+		printf("Enter six integers: \n");
 
+		for(int i = 1; i <= 6; i = i + 1) {
+
+			if(i == 1) {
+				scanf("%d", &a);
+			}else if(i == 2) {
+				scanf("%d", &b);
+			}else if(i == 3) {
+				scanf("%d", &c);
+			}else if(i == 4) {
+				scanf("%d", &d);
+			}else if(i == 5) {
+				scanf("%d", &e);
+			}else{
+				scanf("%d", &f);
+			}
+
+		}
+
+		printf("1234567890bb1234567890\n");
+		printf("%10d  %10d\n", a, b);
+		printf("%10d  %10d\n", c, d);
+		printf("%10d  %10d\n", e, f);
 	}
