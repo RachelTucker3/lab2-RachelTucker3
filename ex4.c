756da4ab105e2da8907ab5e54b3febd182d5d855
Rachel Tucker <rach1234@live.unc.edu>
2019-02-04 02:06:37 +0000

    replace this with a useful message

---
+++ ex4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+	int main()
+	{
+
+	int a, b, c, d, e, f;
+
+	printf("Enter six integers: \n");
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	}
---
+++ ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+	int main()
+	{
+
+	int x;
+
+	printf("Number ? \n");
+	scanf("%d", &x);
+
+	}
