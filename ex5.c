54bef964361dd3e7a98c819c3ac7c8294190ade5
Rachel Tucker <rach1234@live.unc.edu>
2019-02-04 17:23:26 +0000

    replace this with a useful message

--- ex5.c
+++ ex5.c
@@ -3,9 +3,30 @@
 	int main()
 	{
 
-	int x;
+		int x;
 
-	printf("Number ? \n");
-	scanf("%d", &x);
+		while(x != 0) {
+
+			printf("Number ? \n");
+			scanf("%d", &x);
+			if(x == 0) {
+			printf("Done\n");
+
+			}else{
+
+				if(((x*(x+1))/2) == (int)((x*(x+1))/2) ) {
+					printf("%d is a triangular number\n", x);
+				} else{
+					printf("%d is not triangular,", x);
+
+					int y = x;
+					while(((y*(y+1))/2) != (int)((y*(y+1))/2)) {
+						y = y - 1;
+					}
+					printf(" nearest trianglular number below %d is %d\n", x, y);
+				}
+
+			}
+		}
 
 	}
